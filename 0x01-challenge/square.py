625ac8de704f9e931b1b2da074c78225d3edcac4
Shiftyshadows <purple.quasar@proton.me>
2023-12-13 23:00:55 +0300

    1. My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 
 
-class Square():
+class Square:
 
-    width = 0
-    height = 0
-
-
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
         """ Area of the square """
@@ -21,8 +17,8 @@ class Square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
