7a65048c97d0b0d9b7b65375c08a6bacd81eced2
Shiftyshadows <purple.quasar@proton.me>
2024-04-03 19:45:54 +0300

    First Commit

---
+++ 0x00-challenge/1-print_square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
+
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
+}
+
+const size = parseInt(process.argv[2], 10)
+
+for (let i = 0 ; i < size ; i ++) {
+    let line = '';
+    for (let j = 0 ; j < size ; j ++) {
+        line += "#";
+    }
+    console.log(line);
+}
